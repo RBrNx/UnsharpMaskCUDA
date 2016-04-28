768f03cda84955fc22b59371804ff1ce24ade7cc
RBrNx277 <conor.watson94@gmail.com>
2016-04-28 17:28:35 +0100

    Refactored code into CUDA Header and CU files

--- UnsharpMaskCUDA/unsharp_mask.cu
+++ UnsharpMaskCUDA/unsharp_mask.cu
@@ -4,62 +4,8 @@
 #include <cuda_runtime.h>
 #include <cuda_occupancy.h>
 #include "ppm.hpp"
-
-// Calculates the weighted sum of two arrays, in1 and in2 according
-// to the formula: out(I) = saturate(in1(I)*alpha + in2(I)*beta + gamma)
-template <typename T>
-__global__ void add_weighted(unsigned char *out, unsigned char *in1, const T alpha, const unsigned char *in2, const T  beta, const T gamma, const unsigned w, const unsigned h, const unsigned nchannels) {
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-
-
-	if (x < w && y < h){
-		unsigned byte_offset = (y*w + x)*nchannels;
-
-		T tmp = in1[byte_offset + 0] * alpha + in2[byte_offset + 0] * beta + gamma;
-		out[byte_offset + 0] = tmp < 0 ? 0 : tmp > UCHAR_MAX ? UCHAR_MAX : tmp;
-
-		tmp = in1[byte_offset + 1] * alpha + in2[byte_offset + 1] * beta + gamma;
-		out[byte_offset + 1] = tmp < 0 ? 0 : tmp > UCHAR_MAX ? UCHAR_MAX : tmp;
-
-		tmp = in1[byte_offset + 2] * alpha + in2[byte_offset + 2] * beta + gamma;
-		out[byte_offset + 2] = tmp < 0 ? 0 : tmp > UCHAR_MAX ? UCHAR_MAX : tmp;
-	}
-
-}
-
-// Averages the nsamples pixels within blur_radius of (x,y). Pixels which
-// would be outside the image, replicate the value at the image border.
-__device__ void pixel_average(unsigned char *out, const unsigned char *in, const int x, const int y, const int blur_radius, const unsigned w, const unsigned h, const unsigned nchannels) {
-	float red_total = 0, green_total = 0, blue_total = 0;
-	const unsigned nsamples = (blur_radius * 2 - 1) * (blur_radius * 2 - 1);
-
-	for (int j = y - blur_radius + 1; j < y + blur_radius; ++j) {
-		for (int i = x - blur_radius + 1; i < x + blur_radius; ++i) {
-			const unsigned r_i = i < 0 ? 0 : i >= w ? w - 1 : i;
-			const unsigned r_j = j < 0 ? 0 : j >= h ? h - 1 : j;
-			unsigned byte_offset = (r_j*w + r_i)*nchannels;
-			red_total += in[byte_offset + 0];
-			green_total += in[byte_offset + 1];
-			blue_total += in[byte_offset + 2];
-		}
-	}
-
-	unsigned byte_offset = (y*w + x)*nchannels;
-	out[byte_offset + 0] = red_total / nsamples;
-	out[byte_offset + 1] = green_total / nsamples;
-	out[byte_offset + 2] = blue_total / nsamples;
-}
-
-__global__ void blur(unsigned char *out, const unsigned char *in, const int blur_radius, const unsigned w, const unsigned h, const unsigned nchannels) {
-	
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-
-	if (x < w && y < h){
-		pixel_average(out, in, x, y, blur_radius, w, h, nchannels);
-	}
-}
+#include "add_weighted.cuh"
+#include "blur.cuh"
 
 void unsharp_mask(unsigned char *out, const unsigned char *in, const int blur_radius, const unsigned w, const unsigned h, const unsigned nchannels) {
 	
@@ -84,8 +30,6 @@ void unsharp_mask(unsigned char *out, const unsigned char *in, const int blur_ra
 	blur<<<gridSize, blockSize>>>(d_blur1, d_in1, blur_radius, w, h, nchannels);
 	blur<<<gridSize, blockSize>>>(d_blur2, d_blur1, blur_radius, w, h, nchannels);
 	blur<<<gridSize, blockSize>>>(d_blur3, d_blur2, blur_radius, w, h, nchannels);
-	
-	//unsigned char *d_blur3; cudaMalloc((void**)&d_blur3, imageSize); cudaMemcpy(d_blur3, d_blur3, blur3.size(), cudaMemcpyHostToDevice);
 	
 	add_weighted<<<gridSize, blockSize>>>(d_out, d_in1, 1.5f, d_blur3, -0.5f, 0.0f, w, h, nchannels);
 	cudaMemcpy(out, d_out, imageSize, cudaMemcpyDeviceToHost);
